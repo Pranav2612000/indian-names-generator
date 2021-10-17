1428bf8e46083f23e92dda60059769f2b6fa97c4
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-10-17 19:11:45 +0530

    chore: make the application deployable on serverless cloud

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { http } = require("@serverless/cloud");
 const namesRoutes = require('./routes/namesRouter');
+
 
 const port = process.env.PORT || 5000;
 
@@ -11,6 +13,9 @@ app.use("/", namesRoutes);
 app.listen(port, () => {
   console.log(`Application up at port ${port}`);
 });
+if(process.env.NODE_ENV != "dev") {
+  http.use(app);
+}
 module.exports = app;
 
 
